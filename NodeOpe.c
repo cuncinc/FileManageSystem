a81febf45ca4db6b4b87ec258939a5fb2625715f
Sinmitom <1509783373@qq.com>
2019-12-11 22:05:06 +0800

    实现了NodeOpe.c中的所有函数

--- NodeOpe.c
+++ NodeOpe.c
@@ -1,50 +1,246 @@
 /*
- * timeï¼š2019å¹´12æœˆ7æ—¥ 11ç‚¹41åˆ†
- * authorï¼šå®‹æ·³
- * æè¿°ï¼šè¦å°è£…çš„æ“ä½œï¼Œä¸å¯¹å¤–æš´éœ²çš„ç»†èŠ‚
+ * time£º2019Äê12ÔÂ7ÈÕ 11µã41·Ö
+ * author£ºËÎ´¾
+ * ÃèÊö£ºÒª·â×°µÄ²Ù×÷£¬²»¶ÔÍâ±©Â¶µÄÏ¸½Ú
 */
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//»ñÈ¡ÎÄ¼ş×´Ì¬ËùĞèµÄÍ·ÎÄ¼ş 
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include<dirent.h>
 
+#include"NodeInfo.h"
+#include"State.h"
+#define maxlen 50
 
-
-//å®ç°æ¯ä¸ªå‡½æ•°éƒ½è¦å†™æ³¨é‡Šï¼Œä¸‹é¢æ—¶ä¸€ä¸ªä¾‹å­ï¼š
 /*
- * Author: å®‹æ·³
- * ä½œç”¨ï¼šè¿”å›æ–‡ä»¶çš„å¤§å°ï¼Œå•ä½ä¸ºByte(å­—èŠ‚)
- * ç”¨æ³•ï¼šå‚æ•°stræ˜¯æ–‡ä»¶çš„è·¯å¾„ï¼Œæ–‡ä»¶ä¸å­˜åœ¨è¿”å›-1
+ * Author: Ğ»ÎÄèº
+ * ×÷ÓÃ£º·µ»ØÎÄ¼ş»ù±¾ÃûµÄÖ¸Õë
+ * ÓÃ·¨£º²ÎÊıpathÊÇÎÄ¼şµÄ¾ø¶ÔÂ·¾¶£¬ÎÄ¼ş²»´æÔÚ·µ»ØNULL
  */
-long get_file_size(char *str)
+char * get_base_name(char * path)
+{
+    if(path == NULL)  //ÈôÂ·¾¶²»´æÔÚ·µ»ØNULL 
+    {
+    	return NULL;
+	}
+	char *name = (char *)malloc(maxlen);
+	int idx; //Î»ÖÃ±ê¼Ç 
+	int i, j;
+	//ÌáÈ¡³öÂ·¾¶Î²µÄÎÄ¼şÃû 
+	for(i = strlen(path); path[i] != '\\'; i--)
+	{
+		idx = i;
+	}
+	for(i = idx, j=0; path[i] != '\0'; i++, j++)
+	{
+		name[j] = path[i];
+	}
+	name[j] = '\0';
+	if(file_exsists(path))  //Èç¹ûÊÇÎÄ¼şÔòìî³ıºó×ºÃû 
+	{
+		for(i = strlen(name); name[i] != '.'; i--)
+		{
+			name[i] = '\0';
+		}
+		name[i] = '\0';	
+	}
+	return name;
+
+}
+
+/*
+ * Author: Ğ»ÎÄèº
+ * ×÷ÓÃ£º·µ»ØÎÄ¼şÍØÕ¹ÃûµÄÖ¸Õë 
+ * ÓÃ·¨£º²ÎÊıpathÊÇÎÄ¼şµÄ¾ø¶ÔÂ·¾¶£¬ÍØÕ¹Ãû²»´æÔÚ·µ»ØNULL
+ */
+char *get_extension(char *path)
+{
+	if(path == NULL || strchr(path, '.') == NULL)  //ÈôÂ·¾¶²»´æÔÚ»òºó×ºÃû²»´æÔÚÔò·µ»ØNULL 
+    {
+    	return NULL;
+	}
+	char *extension = (char *)malloc(maxlen);
+	int idx; //Î»ÖÃ±ê¼Ç 
+	int i, j;
+	//ÌáÈ¡³öÂ·¾¶Î²µÄÎÄ¼şÍØÕ¹Ãû 
+	for(i = strlen(path); path[i] != '.'; i--)
+	{
+		idx = i;
+	}
+	for(i = idx, j=0; path[i] != '\0'; i++, j++)
+	{
+		extension[j] = path[i];
+	}
+	extension[j] = '\0';
+	return extension;
+		
+} 
+
+
+
+/*
+ * Author: ËÎ´¾  Modify:Ğ»ÎÄèº 
+ * ×÷ÓÃ£º·µ»ØÎÄ¼şµÄ´óĞ¡£¬µ¥Î»ÎªByte(×Ö½Ú)
+ * ÓÃ·¨£º²ÎÊıpathÊÇÎÄ¼şµÄÂ·¾¶£¬ÎÄ¼ş²»´æÔÚ·µ»Ø-1
+ */
+long get_file_size(char *path)
 {
     long size;
-    FILE* f = fopen(str, "rb");
+    FILE* f = fopen(path, "rb");
     fseek(f, 0L, SEEK_END);
     size = ftell(f);
     fclose(f);
     return size;
 }
 
+/*
+ * Author: ËÎ´¾  Modify:Ğ»ÎÄèº
+ * ×÷ÓÃ£º»ñÈ¡ÎÄ¼şµ±Ç°×´Ì¬
+ * ÓÃ·¨£º²ÎÊıpathÊÇÎÄ¼şµÄÂ·¾¶,´òÓ¡³öÎÄ¼şµÄÏà¹ØĞÅÏ¢
+*/
+void get_file_status(char *path)
+ {
+     struct _stat buf;
+     int result;
+     //»ñµÃÎÄ¼ş×´Ì¬ĞÅÏ¢
+     result = _stat(path , &buf );
+     //ÏÔÊ¾ÎÄ¼ş×´Ì¬ĞÅÏ¢
+    if( result != 0 )
+        printf( "ÏÔÊ¾ÎÄ¼ş×´Ì¬ĞÅÏ¢³ö´í" );//²¢ÌáÊ¾³ö´íµÄÔ­Òò£¬ÈçNo such file or directory£¨ÎŞ´ËÎÄ¼ş»òË÷Òı£©
 
-// è¿™æ˜¯ä¸€ä¸ªè·å–æ–‡ä»¶çŠ¶æ€çš„ä¾‹å­å’Œæ–¹æ³•
-// /*
-//  *  éœ€è¦åŠ çš„å¤´æ–‡ä»¶
-//  *  #include <time.h>
-//     #include <sys/types.h>
-//     #include <sys/stat.h>
-//  */
-// void get_file_status(char *path)
-// {
-//     struct stat buf;
-//     int result;
-//     //è·å¾—æ–‡ä»¶çŠ¶æ€ä¿¡æ¯
-//     result =stat(path , &buf );
-//     //æ˜¾ç¤ºæ–‡ä»¶çŠ¶æ€ä¿¡æ¯
-//    if( result != 0 )
-//        perror( "æ˜¾ç¤ºæ–‡ä»¶çŠ¶æ€ä¿¡æ¯å‡ºé”™" );//å¹¶æç¤ºå‡ºé”™çš„åŸå› ï¼Œå¦‚No such file or directoryï¼ˆæ— æ­¤æ–‡ä»¶æˆ–ç´¢å¼•ï¼‰
+     else
+     {
+         printf("ÎÄ¼ş´óĞ¡: %d", buf.st_size);
+         printf("\nÎÄ¼ş´´½¨Ê±¼ä: %s", ctime(&buf.st_ctime));
+         printf("·ÃÎÊÈÕÆÚ: %s", ctime(&buf.st_atime));
+         printf("×îºóĞŞ¸ÄÈÕÆÚ: %s", ctime(&buf.st_mtime));
+     }
+ }
 
-//     else
-//     {
-//         printf("æ–‡ä»¶å¤§å°: %d", buf.st_size);
-//         printf("æ–‡ä»¶åˆ›å»ºæ—¶é—´: %s", ctime(&buf.st_ctime));
-//         printf("è®¿é—®æ—¥æœŸ: %s", ctime(&buf.st_atime));
-//         printf("æœ€åä¿®æ”¹æ—¥æœŸ: %s", ctime(&buf.st_mtime));
-//     }
-// }
+/*
+ * Author: Ğ»ÎÄèº
+ * ×÷ÓÃ£ºÅĞ¶ÏÂ·¾¶ÖÕµãÊÇ·ñÎªÎÄ¼ş
+ * ÓÃ·¨£º²ÎÊıpathÊÇÎÄ¼şµÄÂ·¾¶,Èç¹ûÊÇÎÄ¼şÔò·µ»Øtrue£¬·ñÔò·µ»Øfalse
+*/
+boolean file_exsists(char *path)
+{
+
+	struct _stat buf;
+	int result;
+	//»ñµÃÎÄ¼ş×´Ì¬ĞÅÏ¢
+	result = _stat(path, &buf);
+	if(_S_IFREG & buf.st_mode) //Èç¹ûÊÇÎÄ¼ş£¬·µ»Øtrue
+	{
+		return true;
+	}
+	else  //Èç¹û²»ÊÇÎÄ¼ş»òÆäËûÇé¿ö£¬·µ»Øfalse
+	{
+		return false;
+	}
+}
+
+/*
+ * Author: Ğ»ÎÄèº
+ * ×÷ÓÃ£ºÅĞ¶ÏÂ·¾¶ÖÕµãÊÇ·ñÎªÎÄ¼ş¼Ğ
+ * ÓÃ·¨£º²ÎÊıpathÊÇÎÄ¼ş¼ĞµÄÂ·¾¶,Èç¹ûÊÇÎÄ¼ş¼ĞÔò·µ»Øtrue£¬·ñÔò·µ»Øfalse
+*/
+boolean folder_exsists(char *path)
+{
+	struct _stat buf;
+	int result;
+	//»ñµÃÎÄ¼ş×´Ì¬ĞÅÏ¢
+	result = _stat(path, &buf);
+	if(_S_IFDIR & buf.st_mode) //Èç¹ûÊÇÎÄ¼ş¼Ğ£¬·µ»Øtrue
+	{
+		return true;
+	}
+	else  //Èç¹û²»ÊÇÎÄ¼ş¼Ğ»òÆäËûÇé¿ö£¬·µ»Øfalse
+	{
+		return false;
+	}
+
+}
+
+/*
+ * Author: Ğ»ÎÄèº
+ * ×÷ÓÃ£º»ñÈ¡Â·¾¶ÎÄ¼şÏà¹ØĞÅÏ¢
+ * ÓÃ·¨£º²ÎÊıpathÊÇÎÄ¼şµÄÂ·¾¶,Â·¾¶²»´æÔÚÊ±·µ»ØNULL
+*/
+FileInfo * create_info_node(char * path)
+{
+	struct _stat buf;
+	int result;
+	int filenum = 0;
+	result = _stat(path, &buf); //»ñµÃÎÄ¼ş×´Ì¬ĞÅÏ¢ 
+	FileInfo * filehead = (FileInfo *)malloc(sizeof(FileNode));
+	if(filehead == NULL || path == NULL || result != 0)// ÈôÄÚ´æ·ÖÅäÊ§°Ü£¬Â·¾¶²»´æÔÚ»òÎÄ¼ş²»´æÔÚ£¬·µ»ØNULL 
+	{
+		return NULL;
+	}
+	char *modify = ctime(&buf.st_mtime);
+	filehead->modifyTime = modify;   //»ñµÃĞŞ¸ÄÊ±¼ä 	
+	//filehead->modifyTimeNum = buf.st_mtime;  //»ñµÃĞŞ¸ÄÊ±¼ä 
+	strcpy(filehead->modifyTime, modify); 
+	filehead->path = path;
+	filehead->baseName = get_base_name(path);
+	if(file_exsists(path))  //Èç¹ûÊÇÎÄ¼ş
+	{
+		filehead->type = file;
+		filehead->size = buf.st_size;
+		filehead->extension = get_extension(path);
+		filehead->innerFileNum = -1;
+	 } 
+	else if(folder_exsists(path)) //Èç¹ûÊÇÎÄ¼ş¼Ğ
+	{
+		filehead->type = folder;
+		filehead->size = -1;
+		filehead->extension = NULL;
+		struct dirent *entry;
+    	DIR *dir = NULL;
+    	dir = opendir(path);
+		while(entry = readdir(dir))
+		{		
+			filenum++;  //ÎÄ¼şÊıÄ¿¼Ó¼Ó
+		}
+		closedir(dir);
+		filehead->innerFileNum = filenum;
+	 } 
+	
+	 
+}
+/*
+ * Author: Ğ»ÎÄèº
+ * ×÷ÓÃ£º»ñÈ¡ÎÄ¼ş¾ø¶ÔÂ·¾¶ 
+ * ÓÃ·¨£º²ÎÊıpathÊÇÎÄ¼şµÄÂ·¾¶,Â·¾¶²»´æÔÚÊ±·µ»ØNULL
+ * ±¸×¢£º´Ëº¯Êı¿ÉÄÜÓĞbug 
+*/
+
+char * get_dir(char * path)
+{
+	if(path == NULL)
+	{
+		return NULL;
+	}
+	char *absolut_path = (char*) malloc (maxlen);
+	int i, j ;
+	for(i = 0, j=0; i<strlen(path); i++, j++)
+	{
+		absolut_path[i] = path[i];
+		if(path[i] == '\\')
+		{
+			absolut_path[j+1] = '\\';
+			j++; 
+		}
+		
+	}
+	absolut_path[j-2] = '\0';
+	printf("%s", absolut_path); 
+	return absolut_path; 
+	
+}
+
+ 
---
+++ test_NodeOpe.c
@@ -0,0 +1,51 @@
+#include"NodeOpe.c"
+//²âÊÔNodeOpe.cÎÄ¼şÀïº¯ÊıÕıÈ·ĞÔ
+//Author:Ğ»ÎÄèº
+//2019.12.11 
+int main()
+{
+	char *path1 = "D:\\×ÀÃæ\\FileManageSystem\\test.c"; //ÎÄ¼ş
+	char *path2 = "D:\\×ÀÃæ\\FileManageSystem"; //ÎÄ¼ş¼Ğ 
+	
+//	²âÊÔget_base_name()º¯Êı
+//	char *name  = get_base_name(path2);
+//	printf("%s", name);	 
+	
+//	²âÊÔget_extension()º¯Êı
+//	char *extension = get_extension(path2);
+//	printf("%s", extension);
+	
+//	²âÊÔget_file_size()º¯Êı 
+//	long int size  = get_file_size(path1);
+//	printf("%ld", size);	
+	
+//	²âÊÔget_file_status()º¯Êı
+//	get_file_status(path1);	
+	
+//	²âÊÔfile_exsists()º¯Êı
+//	printf("%d %d", file_exsists(path1), file_exsists(path2)); 
+
+//	²âÊÔfolder_exsists()º¯Êı
+//	printf("%d %d", folder_exsists(path1), folder_exsists(path2)); 
+
+//	²âÊÔget_dir()º¯Êı
+// 	printf("%s", get_dir(path1)); 
+
+	
+//	²âÊÔcreate_info_node()º¯Êı
+//	FileInfo * head =  create_info_node(path2);
+//	printf("%d\n", head->type);
+//	printf("%s\n", head->baseName);
+//	printf("%s\n", head->path);
+//	printf("%ld\n", head->modifyTimeNum);
+//	printf("%s\n", head->modifyTime);
+//	printf("%ld\n", head->size);
+//	printf("%s\n", head->extension);
+//	printf("%d\n", head->innerFileNum);		
+	
+	
+	
+	
+	return 0;
+}
+
