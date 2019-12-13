48f1273121e0ac461dab640da9fe12040153ccc9
guoxiatong <88099738@qq.com>
2019-12-13 21:29:31 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/cuncinc/FileManageSystem""
    This reverts commit 6e38a3e4718ac9aa266f6b60ce32a57bbb046a7b.

--- BiTreeOpe.h
+++ BiTreeOpe.h
@@ -1,13 +1,13 @@
 /*
- * time：2019年12月7日 11点38分
- * autho：宋淳
- * 描述：定义了对存储文件的二叉树的各种操作，
- *      理论上应只对外暴露这些函数的声明，屏蔽内部实现。
- *      在设计GUI时和命令行控制时也应只调用这些函数。
+ * time��2019��12��7�� 11��38��
+ * autho���δ�
+ * �����������˶Դ洢�ļ��Ķ������ĸ��ֲ�����
+ *      ������Ӧֻ���Ⱪ¶��Щ�����������������ڲ�ʵ�֡�
+ *      �����GUIʱ�������п���ʱҲӦֻ������Щ������
  */
 
-/*modify: 谢文韬*/
-//实现这个头文件时，要引入InfoOpe.h，可以使用里面的函数
+/*modify: л���*/
+//ʵ�����ͷ�ļ�ʱ��Ҫ����InfoOpe.h������ʹ������ĺ���
 
 
 #ifndef BITREEOPE_H
@@ -16,31 +16,31 @@
 #include "NodeInfo.h"
 #include "State.h"
 
-// 没考虑清楚，必须要传入2个参数才行
+// û�������������Ҫ����2����������
 // /*
-//  * 实现功能：分别从path得到与之对应的FileNode和从FileNode得到对应的path
+//  * ʵ�ֹ��ܣ��ֱ��path�õ���֮��Ӧ��FileNode�ʹ�FileNode�õ���Ӧ��path
 //  *
 // */
 // FileNode * get_node_from_path(char *path);
 // char * get_path_from_node(FileNode *node);
 
 /*
- * 功能描述：创建path目录下的二叉树
- * 实现要求：
- *      1. 用左孩子右兄弟法存储节点
- *      2. 应先判断此path是否为空，若为空，直接返回NULL
- *      3. 递归创建此path目录下的其他文件夹的节点
- *      4. 可调用creat_node函数
+ * ��������������pathĿ¼�µĶ�����
+ * ʵ��Ҫ��
+ *      1. ���������ֵܷ��洢�ڵ�
+ *      2. Ӧ���жϴ�path�Ƿ�Ϊ�գ���Ϊ�գ�ֱ�ӷ���NULL
+ *      3. �ݹ鴴����pathĿ¼�µ������ļ��еĽڵ�
+ *      4. �ɵ���creat_node����
  */
 FilesBiTree create_files_bitree(char *path);
 
 
 
 /*
- * 实现功能：在node文件夹下新建一个名称为fileName的文件
- * 实现要求：
- *      1. 检查node是否为文件夹，检查fileName是否已存在
- *      2. 先新建实际的文件，再创建此文件二叉树节点，把节点插到node的左孩子
+ * ʵ�ֹ��ܣ���node�ļ������½�һ������ΪfileName���ļ�
+ * ʵ��Ҫ��
+ *      1. ���node�Ƿ�Ϊ�ļ��У����fileName�Ƿ��Ѵ���
+ *      2. ���½�ʵ�ʵ��ļ����ٴ������ļ��������ڵ㣬�ѽڵ�嵽node������
  */
 int add_file(FileNode *node, char *fileName);
 int add_file_in_path(char *path, char *fileName);
@@ -48,8 +48,8 @@ int add_file_in_path(char *path, char *fileName);
 
 
 /*
- * 实现功能：在node文件夹下新建一个名为folderName的文件夹
- * 要求：同上
+ * ʵ�ֹ��ܣ���node�ļ������½�һ����ΪfolderName���ļ���
+ * Ҫ��ͬ��
 */
 int add_folder(FileNode *node, char *folderName);
 int add_folder_in_path(char *path, char *folderName);
@@ -57,39 +57,39 @@ int add_folder_in_path(char *path, char *folderName);
 
 
 /*
- * 返回类型：OK表示成功执行，ERROR表示执行失败
- * 实现功能：打印此目录下的文件(夹)，参考在powerShell中使用tree命令的效果
- * 实现要求：
- *          1. 要先判断接收到的是文件还是文件夹
- *          2. 若文件夹为空，则直接返回
- *          3. 递归调用，输出此文件夹下的所有文件(夹)
+ * �������ͣ�OK��ʾ�ɹ�ִ�У�ERROR��ʾִ��ʧ��
+ * ʵ�ֹ��ܣ���ӡ��Ŀ¼�µ��ļ�(��)���ο���powerShell��ʹ��tree�����Ч��
+ * ʵ��Ҫ��
+ *          1. Ҫ���жϽ��յ������ļ������ļ���
+ *          2. ���ļ���Ϊ�գ���ֱ�ӷ���
+ *          3. �ݹ���ã�������ļ����µ������ļ�(��)
  */
 int tree_files(FilesBiTree tree);
-int tree_files_in_path(char *path);   //功能同上，接受路径作为参数，可调用print_files函数
+int tree_files_in_path(char *path);   //����ͬ�ϣ�����·����Ϊ�������ɵ���print_files����
 
 
 
 /*
- * 实现功能：只输出此目录下的文件(夹)的名字
- * 实现要求：
- *          1. 要先判断接收到的是文件还是文件夹。
- *          2. 若文件夹为空，则直接返回
- *          3. 只打印名字，一行最多6个
- *          5. **一定要对齐，行列都要，左对齐
- *              思路：先找到最长的字符串的长度，以此长度作为所有列的长度，
- *              同时，若最长长度超过某个值，则用这个值，字符串直接截掉
- *          4. 可调用sort_nodes_by_name()函数将文件排序整齐
+ * ʵ�ֹ��ܣ�ֻ�����Ŀ¼�µ��ļ�(��)������
+ * ʵ��Ҫ��
+ *          1. Ҫ���жϽ��յ������ļ������ļ��С�
+ *          2. ���ļ���Ϊ�գ���ֱ�ӷ���
+ *          3. ֻ��ӡ���֣�һ�����6��
+ *          5. **һ��Ҫ���룬���ж�Ҫ�������
+ *              ˼·�����ҵ�����ַ����ĳ��ȣ��Դ˳�����Ϊ�����еĳ��ȣ�
+ *              ͬʱ��������ȳ���ĳ��ֵ���������ֵ���ַ���ֱ�ӽص�
+ *          4. �ɵ���sort_nodes_by_name()�������ļ���������
  */
 int list_files_name_only(FilesBiTree tree);
-int list_files_name_only_in_path(char *path); //功能同上，接受路径作为参数，可调用以上函数
+int list_files_name_only_in_path(char *path); //����ͬ�ϣ�����·����Ϊ�������ɵ������Ϻ���
 
 
 
 /*
- * 实现功能：在一行中输出一个文件(夹)的文件(夹)命，大小，时间
- * 实验要求：
- *      1. 判断是不是文件夹，为空文件夹直接返回
- *      2. 列与列间要对齐，参考在PowerShell中ls命令的样式
+ * ʵ�ֹ��ܣ���һ�������һ���ļ�(��)���ļ�(��)������С��ʱ��
+ * ʵ��Ҫ��
+ *      1. �ж��ǲ����ļ��У�Ϊ���ļ���ֱ�ӷ���
+ *      2. �����м�Ҫ���룬�ο���PowerShell��ls�������ʽ
  */
 int list_files(FilesBiTree tree);
 int list_files_in_path(char *path);
@@ -97,30 +97,30 @@ int list_files_in_path(char *path);
 
 
 /*
- * 较难！
- * 功能描述：在指定文件夹下查找名字中有name的文件(夹)，
- *          并把节点的指针存储在一个只读链表(数组)中，在UI中显示是需要二级指针
- * 难点：要使用二级指针
- * 优化：效率可能会很低，可以想办法建一个索引
+ * ���ѣ�
+ * ������������ָ���ļ����²�����������name���ļ�(��)��
+ *          ���ѽڵ��ָ��洢��һ��ֻ������(����)�У���UI����ʾ����Ҫ����ָ��
+ * �ѵ㣺Ҫʹ�ö���ָ��
+ * �Ż���Ч�ʿ��ܻ�ܵͣ�������취��һ������
 */
 FileNode **find(FileNode * dir, char *name);
 
 
 
 /*
- * 功能描述：在dirPath文件夹下查找名字中有name的文件(夹)，
- *          并像ls一样打印其详细信息到屏幕上
+ * ������������dirPath�ļ����²�����������name���ļ�(��)��
+ *          ����lsһ����ӡ����ϸ��Ϣ����Ļ��
 */
 int find_and_list(char * dirPath, char *name);
 
 
 
 /*
- * 实现功能：修改此FileNode对应文件(夹)的名字，不仅要在这个系统中修改，还要对此文件的实际名字修改
- * 功能要求：
- *      1. 修改二叉树中存储的信息
- *      2. 修改实际文件信息
- *      3. 要更新节点的信息，就是修改时间那一项
+ * ʵ�ֹ��ܣ��޸Ĵ�FileNode��Ӧ�ļ�(��)�����֣�����Ҫ�����ϵͳ���޸ģ���Ҫ�Դ��ļ���ʵ�������޸�
+ * ����Ҫ��
+ *      1. �޸Ķ������д洢����Ϣ
+ *      2. �޸�ʵ���ļ���Ϣ
+ *      3. Ҫ���½ڵ����Ϣ�������޸�ʱ����һ��
 */
 int rename_file(FileNode *node, char *newName);
 int rename_file_in_path(char *path, char *newName);
@@ -128,15 +128,15 @@ int rename_file_in_path(char *path, char *newName);
 
 
 /*
- * 调用须知：调用此函数时，要先处理好其父节点与其孩子的关系。
- *          同时，在调用要提示用户确认删除
- * 实现功能：把node指向的空间free掉，把实际文件删掉
- *          实现者不用管左右孩子，那是调用者该关心的。
- * 实现要求：
- *      1.判断是文件还是文件夹，若是文件夹直接返回
- *      2.把要删除的节点的动态内存free掉
- *      3.删除二叉树的节点
- *      4.删除实际文件，若权限不够则不理会了
+ * ������֪�����ô˺���ʱ��Ҫ�ȴ������丸�ڵ����亢�ӵĹ�ϵ��
+ *          ͬʱ���ڵ���Ҫ��ʾ�û�ȷ��ɾ��
+ * ʵ�ֹ��ܣ���nodeָ��Ŀռ�free������ʵ���ļ�ɾ��
+ *          ʵ���߲��ù����Һ��ӣ����ǵ����߸ù��ĵġ�
+ * ʵ��Ҫ��
+ *      1.�ж����ļ������ļ��У������ļ���ֱ�ӷ���
+ *      2.��Ҫɾ���Ľڵ�Ķ�̬�ڴ�free��
+ *      3.ɾ���������Ľڵ�
+ *      4.ɾ��ʵ���ļ�����Ȩ�޲�����������
 */
 int remove_file(FileNode *node);
 int remove_file_in_path(char *path);
@@ -144,13 +144,13 @@ int remove_file_in_path(char *path);
 
 
 /*
- * 调用须知：调用此函数前，要把其右孩子赋给其父亲指向它的那个孩子，(左孩子右兄弟法存储)
- *          删除前要用户确认。
- * 实现功能：删除此文件夹下的所有文件，循环free节点。
- * 实现要求：
- *      1.判断是文件夹还是文件，是文件就直接返回
- *      2.free掉所有动态内存
- *      3.删除此节点的左孩子及其所有孩子(其右孩子和它是同一个级别的文件)
+ * ������֪�����ô˺���ǰ��Ҫ�����Һ��Ӹ����丸��ָ�������Ǹ����ӣ�(�������ֵܷ��洢)
+ *          ɾ��ǰҪ�û�ȷ�ϡ�
+ * ʵ�ֹ��ܣ�ɾ�����ļ����µ������ļ���ѭ��free�ڵ㡣
+ * ʵ��Ҫ��
+ *      1.�ж����ļ��л����ļ������ļ���ֱ�ӷ���
+ *      2.free�����ж�̬�ڴ�
+ *      3.ɾ���˽ڵ�����Ӽ������к���(���Һ��Ӻ�����ͬһ��������ļ�)
 */
 int remove_folder(FileNode *node);
 int remove_folder_in_path(char *path);
@@ -158,33 +158,33 @@ int remove_folder_in_path(char *path);
 
 
 /*
- * 实现功能：复制功能
- * 实现步骤：
- *      1. 判断destPath是否为存在的文件夹
- *      2. 若node为folder型，调用copy_folder函数；否则调用copy_file
+ * ʵ�ֹ��ܣ����ƹ���
+ * ʵ�ֲ��裺
+ *      1. �ж�destPath�Ƿ�Ϊ���ڵ��ļ���
+ *      2. ��nodeΪfolder�ͣ�����copy_folder�������������copy_file
 */
 int copy_node(char *destPath, FileNode * node);
-//把绝对路径为sourcePath的文件(夹)复制到destPath文件夹下，实现同上，
-//把sourcePath翻译为相应的node参数，若没有则返回错误代码ERROR
+//�Ѿ���·��ΪsourcePath���ļ�(��)���Ƶ�destPath�ļ����£�ʵ��ͬ�ϣ�
+//��sourcePath����Ϊ��Ӧ��node��������û���򷵻ش������ERROR
 int copy_node_in_path(char *destPath, char *sourcePath);
 
 
 
 /*
- * 实现功能：把node指向的节点复制到目标路径destPath下
- * 实现要求：
- *      1. 把node接到destPath的左孩子，或者左孩子的右孩子上
- *      2. 实际的文件复制操作
+ * ʵ�ֹ��ܣ���nodeָ��Ľڵ㸴�Ƶ�Ŀ��·��destPath��
+ * ʵ��Ҫ��
+ *      1. ��node�ӵ�destPath�����ӣ��������ӵ��Һ�����
+ *      2. ʵ�ʵ��ļ����Ʋ���
 */
 int copy_file(char * destPath, FileNode *node);
 
 
 
 /*
- * 实现功能：把node指向的目录复制到destPath下，
- *          注意这是要复制一整个目录(包括其下的所有文件(夹))到目标路径
- * 实现要求：
- *      1. 判断node是否为目录
+ * ʵ�ֹ��ܣ���nodeָ���Ŀ¼���Ƶ�destPath�£�
+ *          ע������Ҫ����һ����Ŀ¼(�������µ������ļ�(��))��Ŀ��·��
+ * ʵ��Ҫ��
+ *      1. �ж�node�Ƿ�ΪĿ¼
  *      2.
 */
 int copy_folder(char *destPath, FileNode *node);
@@ -192,10 +192,10 @@ int copy_folder(char *destPath, FileNode *node);
 
 
 /*
- * 功能描述：把node文件移动到destPath目录下
- * 实习要求：
- *      1. 判断destPath和node
- * 实现思路：可用copy_file函数和remove_file函数
+ * ������������node�ļ��ƶ���destPathĿ¼��
+ * ʵϰҪ��
+ *      1. �ж�destPath��node
+ * ʵ��˼·������copy_file������remove_file����
 */
 int move_file(char *destPath, FileNode *node);
 int move_file_in_path(char *destPath, char *sourcePath);
@@ -203,15 +203,15 @@ int move_file_in_path(char *destPath, char *sourcePath);
 
 
 /*
- * 实现功能：只对tree目录下的这一层文件进行排序，ordering是排序方式，
- *          有name，modifyTime，size，拓展名(拓展名按字典序排，拓展名相同按name排)，
- *          以及升序降序的方式
- * 实现细节：
- *      1. 判断tree是否为文件夹
- *      2. 对tree的右子树排序
- * 实现思路：可以用switch_case语句解析ordering，函数跳转到特定的排序方式。当然也可以用其他更好的方法。
- * 难点：目前对这个函数的实现没有太好的办法，算是一个难点。如果不能复用代码，每个方式都写一个算法，
- *      再加上升序降序的选择，至少要写8个算法，但是这些算法只是判定的主键不同，造成大量代码复用。
+ * ʵ�ֹ��ܣ�ֻ��treeĿ¼�µ���һ���ļ���������ordering������ʽ��
+ *          ��name��modifyTime��size����չ��(��չ�����ֵ����ţ���չ����ͬ��name��)��
+ *          �Լ�������ķ�ʽ
+ * ʵ��ϸ�ڣ�
+ *      1. �ж�tree�Ƿ�Ϊ�ļ���
+ *      2. ��tree������������
+ * ʵ��˼·��������switch_case������ordering��������ת���ض�������ʽ����ȻҲ�������������õķ�����
+ * �ѵ㣺Ŀǰ�����������ʵ��û��̫�õİ취������һ���ѵ㡣������ܸ��ô��룬ÿ����ʽ��дһ���㷨��
+ *      �ټ����������ѡ������Ҫд8���㷨��������Щ�㷨ֻ���ж���������ͬ����ɴ������븴�á�
 */
 void sort_files(FilesBiTree tree, char *ordering);
 void sort_files_in_path(char *path);
--- NodeOpe.h
+++ NodeOpe.h
@@ -3,50 +3,50 @@
 
 
 /*
- * 字符串操作，从路径种返回文件基本名(除了拓展名的字符串)
- * 如C:\\test.txt返回test
- * 注意要用动态内存
+ * �ַ�����������·���ַ����ļ�������(������չ�����ַ���)
+ * ��C:\\test.txt����test
+ * ע��Ҫ�ö�̬�ڴ�
  */
 char * get_base_name(char * path);
 
 
 
 /*
- * 返回文件的拓展名，没有拓展名就返回NULL
+ * �����ļ�����չ����û����չ���ͷ���NULL
 */
 char * get_extension(char *path);
 
 
 
 /*
- * 作用：返回文件的大小，单位为Byte(字节)
- * 用法：参数str是文件的路径，文件不存在返回-1
+ * ���ã������ļ��Ĵ�С����λΪByte(�ֽ�)
+ * �÷�������str���ļ���·�����ļ������ڷ���-1
  */
 long get_file_size(char *str);
 
 
 
 /*
- * 使用方式：传入一个文件的绝对路径path，返回这个文件的FileInfo节点。注意使用前要判断是否为NULL
- * 实现要求：
- *      1.先判断path是否为文件夹或不存在，是的话直接返回NULL
- *      2.可调用其他已经写好的函数，节点的所有信息都要获取
+ * ʹ�÷�ʽ������һ���ļ��ľ���·��path����������ļ���FileInfo�ڵ㡣ע��ʹ��ǰҪ�ж��Ƿ�ΪNULL
+ * ʵ��Ҫ��
+ *      1.���ж�path�Ƿ�Ϊ�ļ��л򲻴��ڣ��ǵĻ�ֱ�ӷ���NULL
+ *      2.�ɵ��������Ѿ�д�õĺ������ڵ��������Ϣ��Ҫ��ȡ
 */
 FileInfo * create_info_node(char *path);
 
 
 
-boolean is_file_node(FileNode *node);    //如果节点寸的是文件则返回true，否则返回false
-boolean is_folder_node(FileNode *node);  //是文件夹则返回true，否则返回false
-boolean file_exsists(char *path);   //若path不存在或者是文件夹，返回false
-boolean folder_exsists(char *path); //若path不存在或是文件，返回false
+boolean is_file_node(FileNode *node);    //����ڵ������ļ��򷵻�true�����򷵻�false
+boolean is_folder_node(FileNode *node);  //���ļ����򷵻�true�����򷵻�false
+boolean file_exsists(char *path);   //��path�����ڻ������ļ��У�����false
+boolean folder_exsists(char *path); //��path�����ڻ����ļ�������false
 
 
 
-//这个函数好像没什么用
+//�����������ûʲô��
 // /*
-//  * 返回文件所在的路径
-//  * 注意要用动态内存
+//  * �����ļ����ڵ�·��
+//  * ע��Ҫ�ö�̬�ڴ�
 //  */
 // char * get_dir(char * path);
 
